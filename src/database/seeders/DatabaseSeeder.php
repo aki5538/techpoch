861b56218fdc4418280a31973e30e59f039f355c
aki <h.aki5538@gmail.com>
2026-04-18 22:16:48 +0900

    シーダー変更

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,11 +3,18 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => '管理者',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 1,
+        ]);
     }
 }
